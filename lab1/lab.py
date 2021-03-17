badf27dc6c98b325036567fafb3d4a18f0f27b84
svyatjes <cvity6692@gmail.com>
2021-03-17 14:05:26 +0300

    add try logic

--- lab1/lab.py
+++ lab1/lab.py
@@ -6,7 +6,9 @@ logging.basicConfig(level=logging.INFO, format=False)
 
 def main():
     try:
-
+        with open("arr.txt") as input_file:
+            arr1 = input_file.readline().split()
+            arr2 = input_file.readline().split()
     except ():
 
     else:
